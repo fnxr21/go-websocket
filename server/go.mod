bee824e143f881e265b994da9f8b44836214176b
fandi.nxr <fandinur214@gmail.com>
2024-02-02 15:57:25 +0700

    websocket golang2

--- server/go.mod
+++ server/go.mod
@@ -2,6 +2,16 @@ module service-chat
 
 go 1.21.5
 
-require github.com/gorilla/websocket v1.5.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.1
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.6
+)
 
-require golang.org/x/net v0.17.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,62 +1,102 @@
 package main
 
 import (
+	"encoding/json" // Added for JSON encoding
 	"fmt"
-	"log"
 	"net/http"
-``
+	"time"
+
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+var upgrader = websocket.Upgrader{}
 
 func main() {
 
-	setupRoutes()
-	fmt.Println("GO WEBSOCKET")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	initDB()
+
+	r := mux.NewRouter()
+	Update()
+	r.HandleFunc("/ws", handleWebSocket)
+	fmt.Println("run")
+
+	http.ListenAndServe(":8080", r)
 }
 
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndPoint)
+// Database instance
+var db *gorm.DB
+
+// Database setup
+func initDB() {
+	var err error
+	dsn := "root:@tcp(127.0.0.1:3306)/websocket?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err) // Handle errors gracefully in production
+	}
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
+// update for every 3 second
+func Update() {
+	for {
+		user := User{ID: 1, Username: "alex"}
+		db.Create(user)
+		time.Sleep(3 * time.Second)
+		fmt.Println("is work ")
+		break
+	}
 
 }
 
-func wsEndPoint(w http.ResponseWriter, r *http.Request) {
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+func RepoQuery() ([]User, error) {
+	user := []User{}
+	err := db.Find(&user).Error // Use `&users` to pass a pointer for modification
+	return user, err
+}
+
+// WebSocket handler
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
-
 	}
 
-	ws, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
+		return
 	}
-	log.Println("Client Successfully Connected . . .")
 
-	reader(ws)
-}
+	defer conn.Close()
 
-func reader(conn *websocket.Conn) {
 	for {
-		messageType, p, err := conn.ReadMessage()
+		data, err := RepoQuery()
 		if err != nil {
-			log.Println(err)
-			return
+			fmt.Println(err)
+			break
 		}
-		log.Println(string(p))
 
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
+		// Marshal data to JSON for WebSocket transmission
+		bytes, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println(err)
+			break
 		}
+
+		err = conn.WriteMessage(websocket.TextMessage, bytes)
+
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		time.Sleep(3 * time.Second)
+		break
 	}
 }
